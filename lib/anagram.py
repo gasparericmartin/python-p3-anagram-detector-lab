0ed6cb0e9ec3260b2b06576623b613e096bc3795
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-03-20 17:44:21 -0400

    Complete assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
-# your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self._word = word
+    
+    def match(self, list):
+        output_list = []
+        
+        for comparison in list:
+            # word_list = [letter for letter in self._word]
+            # comparison_list = [letter for letter in comparison]
+
+            if sorted([letter for letter in self._word]) == \
+                sorted([letter for letter in comparison]):
+                output_list.append(comparison)
+
+        return output_list
+
